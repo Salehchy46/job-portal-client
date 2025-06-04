2b660931918877e6429a80ec746cbe2840c935a5
salehchy46 <salehchyctg@gmail.com>
2025-06-04 13:06:33 +0600

    Amar routing and pages and layout add korlam

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,11 +1,20 @@
 import {
   createBrowserRouter,
 } from "react-router-dom";
+import Mainlayout from "../layout/Mainlayout";
+import Home from "../pages/Home/Home";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <div>Hellow!</div>
+        element: <Mainlayout />,
+        errorElement: <h1 className="text-center text-3xl min-h-dvh items-center">Route Not Found</h1>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>,
+            }
+        ]
     },
 ]);
 
