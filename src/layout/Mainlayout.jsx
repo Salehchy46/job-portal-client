400bfef8cdfc18d79cb2df0d4d22268fbe55e531
salehchy46 <salehchyctg@gmail.com>
2025-06-04 20:10:43 +0600

    login page is added

--- src/layout/Mainlayout.jsx
+++ src/layout/Mainlayout.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import Navbar from '../pages/shared/Navbar';
 
 const Mainlayout = () => {
     return (
         <div className='max-w-7xl mx-auto'>
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,6 +3,7 @@ import {
 } from "react-router-dom";
 import Mainlayout from "../layout/Mainlayout";
 import Home from "../pages/Home/Home";
+import Register from "../pages/Register/Register";
 
 const router = createBrowserRouter([
     {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>,
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
             }
         ]
     },
