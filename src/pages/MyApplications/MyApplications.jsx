a9f85eb327de72eadbbec0c0a02410568a635dc2
salehchy46 <salehchyctg@gmail.com>
2025-06-16 03:24:11 +0600

    Applied job Table is added

--- src/pages/MyApplications/MyApplications.jsx
+++ src/pages/MyApplications/MyApplications.jsx
@@ -13,8 +13,51 @@ const MyApplications = () => {
     }, [user.email])
 
     return (
-        <div>
-            <h2 className="text-3xl font-bold text-center my-10">My Applications: {jobs.length}</h2>
+        <div className='my-10'>
+            <h2 className="text-3xl font-bold text-center mb-10">My Applications: {jobs.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Company</th>
+                            <th>Job Type</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* Jobs list */}
+                        {
+                            jobs.map(job => <tr key={job._id}>
+                            <td>
+                                <div className="flex items-center gap-3">
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle h-12 w-12">
+                                            <img
+                                                src={job.company_logo}
+                                                alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="font-bold">{job.title}</div>
+                                        <div className="text-sm opacity-50">{job.location}</div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                {job.company}
+                                <br />
+                            </td>
+                            <td>{job.jobType}</td>
+                            <th>
+                                <button className="btn btn-ghost btn-xs">delete</button>
+                            </th>
+                        </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
