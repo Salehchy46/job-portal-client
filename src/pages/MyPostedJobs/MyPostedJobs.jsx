8e228d057ed11abc5e7a36df3436761de1898269
salehchy46 <salehchyctg@gmail.com>
2025-06-19 01:31:08 +0600

    job add form is completed

--- src/pages/MyPostedJobs/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs/MyPostedJobs.jsx
@@ -4,17 +4,40 @@ import useAuth from '../../hooks/useAuth';
 const MyPostedJobs = () => {
 
     const [jobs, setJobs] = useState([]);
-    const {user} = useAuth();
+    const { user } = useAuth();
 
     useEffect(() => {
         fetch(`http://localhost:3000/jobs?email=${user.email}`)
-        .then(res => res.json())
-        .then(data => setJobs(data))
+            .then(res => res.json())
+            .then(data => setJobs(data))
     }, [user.email])
 
     return (
         <div>
             <h2 className="text-3xl font-bold text-center py-10">My Posted Jobs: {jobs.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>logo</th>
+                            <th>company</th>
+                            <th>Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            jobs.map((job, index) => <tr key={job._id}>
+                                <th>{index + 1}</th>
+                                <img src={job.company_logo} className='rounded-full w-10 h-10' alt="logo" />
+                                <td>{job.company}</td>
+                                <td>{job.title}</td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
