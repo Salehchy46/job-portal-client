1728e572ee338d55d48e1b2d9795791a814775fc
salehchy46 <salehchyctg@gmail.com>
2025-06-25 03:57:05 +0600

    the page for application status update is developed

--- src/pages/ViewApplications/ViewApplications.jsx
+++ src/pages/ViewApplications/ViewApplications.jsx
@@ -1,22 +1,49 @@
 import React from 'react';
 import { useLoaderData } from 'react-router';
+import Swal from 'sweetalert2';
 
 const ViewApplications = () => {
 
     const applications = useLoaderData();
 
+    const handleStatusUpdate = (e, id) => {
+        console.log(e.target.value, id);
+        const data = {
+            status: e.target.value,
+        }
+        fetch(`http://localhost:3000/job-application/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount) {
+                    Swal.fire({
+                        position: "top-start",
+                        icon: "success",
+                        title: "Status has been Updated",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
+    }
+
     return (
         <div>
             <h2 className="text-3xl text-center font-bold py-10">Applications for this job: {applications.length}</h2>
-            <div className="overflow-x-auto">
+            <div className="overflow-x-auto mb-10">
                 <table className="table">
                     {/* head */}
                     <thead>
                         <tr>
                             <th></th>
                             <th>Email</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>Status</th>
+                            <th>Update Status</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -25,7 +52,18 @@ const ViewApplications = () => {
                                 <th>{index + 1}</th>
                                 <td>{app.applicant_email}</td>
                                 <td>Quality Control Specialist</td>
-                                <td>Blue</td>
+                                <td>
+                                    <select
+                                        defaultValue={app.status || 'Change Status'}
+                                        onChange={(e) => handleStatusUpdate(e, app._id)}
+                                        className="select select-xs">
+                                        <option disabled={true}>Change Status</option>
+                                        <option>Under Review</option>
+                                        <option>Set Interview</option>
+                                        <option>Hired</option>
+                                        <option>Rejected</option>
+                                    </select>
+                                </td>
                             </tr>)
                         }
                     </tbody>
