9dd7884d75b99b57e54700739b18494dbef4487f
salehchy46 <salehchyctg@gmail.com>
2025-06-30 02:26:14 +0600

    common side is detected

--- src/context/AuthContext/AuthProvider.jsx
+++ src/context/AuthContext/AuthProvider.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import AuthContext from './AuthContext';
 import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import auth from '../../firebase/firebase.init';
+import axios from 'axios';
 
 const googleProvider = new GoogleAuthProvider();
 
@@ -29,7 +30,14 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             console.log('state captured', currentUser);
+            if(currentUser?.email){
+                const user = {email : currentUser.email}
+
+                axios.post('http://localhost:3000/jwt', user)
+                .then(res => console.log(res.data))
+            }
             
+            //put it in the right place
             setLoading(false);
         })
 
