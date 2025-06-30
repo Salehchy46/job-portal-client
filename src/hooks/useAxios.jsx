e76066903fa786ef21359a5fe5b66aea06a46f35
salehchy46 <salehchyctg@gmail.com>
2025-07-01 02:38:06 +0600

    interceptor is used to logout invalid user

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useEffect } from 'react';
+import useAuth from './useAuth';
+import { useNavigate } from 'react-router';
 
 const axiosInstance = axios.create({
     baseURL: 'http://localhost:3000',
@@ -7,6 +9,31 @@ const axiosInstance = axios.create({
 })
 
 const useAxios = () => {
+
+    const { logOut } = useAuth()
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        axiosInstance.interceptors.response.use(response => {
+            return response;
+        }, error => {
+
+            console.log('error caught in the interceptor', error);
+
+            if (error.status === 401 || error.status === 403) {
+                console.log('User is invalid, log out');
+                logOut()
+                    .then(() => {
+                        console.log('logged out user');
+                        navigate('/signin')
+                    })
+                    .catch(error => console.log(error))
+            }
+
+            return Promise.reject(error);
+        })
+    }, [logOut, navigate])
+
     return axiosInstance;
 };
 
